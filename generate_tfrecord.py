b0a311a9899fc4a43c66b74142f93bbeafb8be80
Maetee L <may3rd@gmail.com>
2021-04-13 21:50:09 +0700

    Add files via upload

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -1,12 +1,13 @@
 """
 Usage:
-  # From tensorflow/models/
-  # Create train data:
-  python generate_tfrecord.py --csv_input=data/train_labels.csv  --output_path=train.record
 
-  # Create test data:
-  python generate_tfrecord.py --csv_input=data/test_labels.csv  --output_path=test.record
+# Create train data:
+python generate_tfrecord.py --label=<LABEL> --csv_input=<PATH_TO_ANNOTATIONS_FOLDER>/train_labels.csv  --output_path=<PATH_TO_ANNOTATIONS_FOLDER>/train.record <PATH_TO_ANNOTATIONS_FOLDER>/label_map.pbtxt
+
+# Create test data:
+python generate_tfrecord.py --label=<LABEL> --csv_input=<PATH_TO_ANNOTATIONS_FOLDER>/test_labels.csv  --output_path=<PATH_TO_ANNOTATIONS_FOLDER>/test.record  --label_map <PATH_TO_ANNOTATIONS_FOLDER>/label_map.pbtxt
 """
+
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
@@ -14,6 +15,7 @@ from __future__ import absolute_import
 import os
 import io
 import pandas as pd
+#import tensorflow as tf
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
 import tensorflow.compat.v1 as tf
@@ -27,8 +29,8 @@ from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
 flags = tf.app.flags
-flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
-flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+flags.DEFINE_string("csv_input", "", "Path to the CSV input")
+flags.DEFINE_string("output_path", "", "Path to output TFRecord")
 flags.DEFINE_string(
     "label_map",
     "",
@@ -38,25 +40,29 @@ flags.DEFINE_string(
 # flags.DEFINE_string('label0', '', 'Name of class[0] label')
 # flags.DEFINE_string('label1', '', 'Name of class[1] label')
 # and so on.
-flags.DEFINE_string('image_dir', '', 'Path to images')
+flags.DEFINE_string("img_path", "", "Path to images")
 FLAGS = flags.FLAGS
 
 
 def split(df, group):
-    data = namedtuple('data', ['filename', 'object'])
+    data = namedtuple("data", ["filename", "object"])
     gb = df.groupby(group)
-    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+    return [
+        data(filename, gb.get_group(x))
+        for filename, x in zip(gb.groups.keys(), gb.groups)
+    ]
 
 
-def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+def create_tf_example(group, path, label_map):
+    with tf.gfile.GFile(os.path.join(path, "{}".format(group.filename)), "rb") as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
     width, height = image.size
 
-    filename = group.filename.encode('utf8')
-    image_format = b'jpg'
+    filename = group.filename.encode("utf8")
+    image_format = b"jpg"
+    # check if the image format is matching with your images.
     xmins = []
     xmaxs = []
     ymins = []
@@ -65,12 +71,12 @@ def create_tf_example(group, path):
     classes = []
 
     for index, row in group.object.iterrows():
-        xmins.append(row['xmin'] / width)
-        xmaxs.append(row['xmax'] / width)
-        ymins.append(row['ymin'] / height)
-        ymaxs.append(row['ymax'] / height)
-        classes_text.append(row['class'].encode('utf8'))
-        classes.append(class_text_to_int(row['class']))
+        xmins.append(row["xmin"] / width)
+        xmaxs.append(row["xmax"] / width)
+        ymins.append(row["ymin"] / height)
+        ymaxs.append(row["ymax"] / height)
+        classes_text.append(row["class"].encode("utf8"))
+        class_index = label_map.get(row["class"])
         assert (
             class_index is not None
         ), "class label: `{}` not found in label_map: {}".format(
@@ -78,20 +84,26 @@ def create_tf_example(group, path):
         )
         classes.append(class_index)
 
-    tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(filename),
-        'image/source_id': dataset_util.bytes_feature(filename),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-    }))
+    tf_example = tf.train.Example(
+        features=tf.train.Features(
+            feature={
+                "image/height": dataset_util.int64_feature(height),
+                "image/width": dataset_util.int64_feature(width),
+                "image/filename": dataset_util.bytes_feature(filename),
+                "image/source_id": dataset_util.bytes_feature(filename),
+                "image/encoded": dataset_util.bytes_feature(encoded_jpg),
+                "image/format": dataset_util.bytes_feature(image_format),
+                "image/object/bbox/xmin": dataset_util.float_list_feature(xmins),
+                "image/object/bbox/xmax": dataset_util.float_list_feature(xmaxs),
+                "image/object/bbox/ymin": dataset_util.float_list_feature(ymins),
+                "image/object/bbox/ymax": dataset_util.float_list_feature(ymaxs),
+                "image/object/class/text": dataset_util.bytes_list_feature(
+                    classes_text
+                ),
+                "image/object/class/label": dataset_util.int64_list_feature(classes),
+            }
+        )
+    )
     return tf_example
 
 
@@ -122,5 +134,5 @@ def main(_):
     print("Successfully created the TFRecords: {}".format(output_path))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     tf.app.run()
